84e7b87d647ca88d43b781988c80e769e6021106
rafael-men <rafaelgoncalves1822@gmail.com>
2025-03-27 16:10:59 -0300

    blocks

---
+++ Tetris-Main/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,            
+                                                
+                                               
+    ResourceDictionaryLocation.SourceAssembly  
+                                                                                    
+)]
---
+++ Tetris-Main/Game Models/BlockQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris_Main.Game_Models.Blocks;
+
+namespace Tetris_Main.Game_Models
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+    }
+}
